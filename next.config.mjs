f6ea8f3738c721a883f61acf51f7ff7afcf922ce
Anatoli1022 <118132724+Anatoli1022@users.noreply.github.com>
2024-07-05 23:43:17 +0300

    feat: adds new prettier , eslint and adds remotePatterns in nextConfig

--- next.config.mjs
+++ next.config.mjs
@@ -5,6 +5,16 @@ const nextConfig = {
   env: {
     NEXT_API_MOVIE_KEY: process.env.NEXT_API_MOVIE_KEY,
   },
+  images: {
+    remotePatterns: [
+      {
+        protocol: 'https',
+        hostname: 'image.tmdb.org',
+        port: '',
+        pathname: '/t/p/**',
+      },
+    ],
+  },
 };
 
 export default nextConfig;
