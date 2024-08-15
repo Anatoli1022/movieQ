2a85c7d1a60fec8f49c2335837a06d150244ba02
Anatoli1022 <118132724+Anatoli1022@users.noreply.github.com>
2024-08-16 02:37:10 +0300

    feat: adds search movieApi

--- src/app/lib/movieApi.ts
+++ src/app/lib/movieApi.ts
@@ -37,3 +37,23 @@ export async function fetchMovieData(endpoint: string, additionalPath: string = 
 
   return res.json();
 }
+
+export async function searchMovieData(query: string) {
+  const apiKey = process.env.NEXT_API_MOVIE_KEY;
+  const options = {
+    method: 'GET',
+    headers: {
+      accept: 'application/json',
+      Authorization: `Bearer ${apiKey}`,
+    },
+  };
+
+  const res = await fetch(`https://api.themoviedb.org/3/search/movie?query=${query}&page=1`, options);
+
+  if (!res.ok) {
+    console.error('Failed to fetch data:', res.status, res.statusText);
+    throw new Error('Failed to fetch data');
+  }
+
+  return res.json();
+}
