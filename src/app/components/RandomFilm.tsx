046579c4c82d65402b7cad2956a1025abf507d46
Anatoli1022 <118132724+Anatoli1022@users.noreply.github.com>
2024-09-08 00:04:58 +0300

    feat: new random film

--- src/app/components/RandomFilm.tsx
+++ src/app/components/RandomFilm.tsx
@@ -1,38 +1,20 @@
 'use client';
 import { useState } from 'react';
-import { fetchMovieData } from '../lib/movieApi';
+import { getRandomMovie } from '../utils/randomMovie';
 import type { Movie } from '@/app/types';
 import { MovieImage } from './shared/movieImage/MovieImage';
 import Link from 'next/link';
 
 const RandomFilm = () => {
-  const arrApi = ['popular', 'top_rated'];
   const [movie, setMovie] = useState<Movie | null>(null);
 
-  const getRandomNum = (max: number) => {
-    return Math.floor(Math.random() * max);
-  };
-
-  480;
-  const getRandom = async () => {
-    const category = arrApi[getRandomNum(arrApi.length)];
-    const randomPage = getRandomNum(480);
-    try {
-      const result = await fetchMovieData(category, '', randomPage);
-      if (result && result.results.length > 0) {
-        const randomMovie = result.results[getRandomNum(result.results.length)];
-        setMovie(randomMovie);
-      }
-    } catch (error) {
-      console.error('Error in Home component:', error);
-    }
-    console.log(movie);
+  const randomMovie = async () => {
+    setMovie(await getRandomMovie());
   };
 
   return (
     <div>
-      
-      <button onClick={getRandom}>Random</button>
+      <button onClick={randomMovie}>Random</button>
       {movie && (
         <Link href={`/movie/${movie.id}`}>
           <h3>{movie.title}</h3>
