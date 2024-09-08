46c7f67bc806dc1086b710182a80e80b47ada723
Anatoli1022 <118132724+Anatoli1022@users.noreply.github.com>
2024-09-08 14:50:10 +0300

    feat: tinder page, random film , next config

--- src/app/components/RandomFilm.tsx
+++ src/app/components/RandomFilm.tsx
@@ -5,16 +5,16 @@ import type { Movie } from '@/app/types';
 import { MovieImage } from './shared/movieImage/MovieImage';
 import Link from 'next/link';
 
-const RandomFilm = () => {
-  const [movie, setMovie] = useState<Movie | null>(null);
+const RandomFilm = ({movie}:any) => {
+  // const [movie, setMovie] = useState<Movie | null>(null);
 
-  const randomMovie = async () => {
-    setMovie(await getRandomMovie());
-  };
+  // const randomMovie = async () => {
+  //   setMovie(await getRandomMovie());
+  // };
 
   return (
     <div>
-      <button onClick={randomMovie}>Random</button>
+      {/* <button onClick={randomMovie}>Random</button> */}
       {movie && (
         <Link href={`/movie/${movie.id}`}>
           <h3>{movie.title}</h3>
